775d8d7ad9012e3d74cf145daef5da57dae757fc
吕思琴 <70061450+roundXin@users.noreply.github.com>
2022-06-16 08:42:02 +0800

    使用formAction

---
+++ common/config.py
@@ -0,0 +1,13 @@
+# NEO4J_CONFIG = {
+#     "uri": "bolt://127.0.0.1:7687",
+#     "auth": ("neo4j", "lsq666666"),
+#
+# }
+NEO4J_CONFIG = {
+    # 'uri': 'bolt://8.142.105.188:7687',
+    'uri': 'bolt://127.0.0.1:7687',
+    # 'auth': ('neo4j', 'cihon'),
+    'auth': ('neo4j', 'neo4j'),
+    'encrypted': False
+}
+# http://180.76.242.191:5566/answerModel/test/%E6%98%AF%E7%9A%84
--- common/dataProcessing.py
+++ common/dataProcessing.py
@@ -1,15 +1,17 @@
 from neo4j import GraphDatabase
 from config import NEO4J_CONFIG
 import numpy as np
+import csv
 import pandas as pd
 
-DEIVER = GraphDatabase.driver(NEO4J_CONFIG)
+DEIVER = GraphDatabase.driver(**NEO4J_CONFIG)
 
 
 class VehicleLabel(object):
     def __init__(self):
         # self.session = DEIVER.session()
-        pass
+        self.a = ''
+
     def getAllData(self):
         """
         获取图谱数据
@@ -24,33 +26,59 @@ class VehicleLabel(object):
         :return:
         """
         with DEIVER.session() as session:
-            cypher = 'match (n:`$unit`) return n.name as name'
-            data = session.run(cypher, unit).data()
+            cypher = 'match (n:`{unit}`) return n.name as name'.format(unit=unit)
+            data = session.run(cypher).data()
             return data
         return null
 
-    def vehicleFault2(self, unit1, ralation1, unit2):
+    def vehicleFaultReason(self, unit1, ralation1, unit2, savepath):
         """
-        车辆状态及故障部件及故障现象、原因部件及原因分析、方案部件及方案分析标注
+        原因部件及原因分析标注
         :return:
         """
         with DEIVER.session() as session:
-            cypher = 'match (n:`$unit1`)-[r:`$ralation1`]->(m:`$unit2`) return n.name as name1,m.name as name2'
-            data = session.run(cypher, unit1, ralation1, unit2).data()
+            cypher = 'match (n:`{unit1}`)-[r:`{ralation1}`]->(m:`{unit2}`) return n.name as name1,m.name as name2'.format(
+                unit1=unit1, ralation1=ralation1, unit2=unit2)
+            data = session.run(cypher).data()
+            data = pd.DataFrame(data).apply(lambda x: '- [' + x[0] + '](reasonUnit)[' + x[1] + '](reasonAnalyse)', axis=1)
+            data.to_csv(savepath,sep='|', header=None, index=None, quoting=csv.QUOTE_NONE,encoding='utf-8')
             return data
         return null
 
-    def vehicleFault3(self, unit1, ralation1, unit2, ralation2, unit3):
+    def vehicleFaultScheme(self, unit1, ralation1, unit2, savepath):
         """
-        车辆状态及故障部件及故障现象、原因部件及原因分析、方案部件及方案分析标注
+        方案部件及方案分析标注
         :return:
         """
         with DEIVER.session() as session:
-            cypher = 'match (l:`$unit1`)-[r:`$ralation1`]->(m:`$unit2`)-[r2:`$ralation2`]->(n:`$unit3`) return l.name as name1,m.name as name2,n.name as name3'
-            data = session.run(cypher, unit1, ralation1, unit2, ralation2, unit3).data()
+            cypher = 'match (n:`{unit1}`)-[r:`{ralation1}`]->(m:`{unit2}`) return n.name as name1,m.name as name2'.format(
+                unit1=unit1, ralation1=ralation1, unit2=unit2)
+            data = session.run(cypher).data()
+            data = pd.DataFrame(data).apply(lambda x: '- [' + x[0] + '](schemeUnit)[' + x[1] + '](schemeAnalyse)', axis=1)
+            data.to_csv(savepath,sep='|', header=None, index=None, quoting=csv.QUOTE_NONE,encoding='utf-8')
             return data
         return null
+
+    def vehicleFaultDetail(self, unit1, ralation1, unit2, ralation2, unit3, savepath):
+        """
+        车辆状态及故障部件及故障现象标注
+        :return:
+        """
+        with DEIVER.session() as session:
+            cypher = 'match (l:`{unit1}`)-[r:`{ralation1}`]->(m:`{unit2}`)-[r2:`{ralation2}`]->(n:`{unit3}`) return l.name as name1,m.name as name2,n.name as name3'.format(
+                unit1=unit1, ralation1=ralation1, unit2=unit2, ralation2=ralation2, unit3=unit3)
+            data = session.run(cypher).data()
+            data = pd.DataFrame(data).apply(
+                lambda x: '- [' + x[0] + '](detailStatus)[' + x[1] + '](detailUnit)[' + x[2] + '](detailPhenomenon)',
+                axis=1)
+            data.to_csv(savepath,sep='|', header=None, index=None, quoting=csv.QUOTE_NONE,encoding='utf-8')
+            return data
+        return null
+
 
 if __name__ == '__main__':
     v = VehicleLabel()
-    vehicleFault
+    r = v.vehicleFaultDetail('车辆状态', '状态部件', '故障部件', '部件现象', '故障现象', './data/故障描述.txt')
+    r = v.vehicleFaultScheme('原因部件', '分析', '原因分析', './data/故障原因.txt')
+    r = v.vehicleFaultReason('方案操作', '维修', '方案部件', './data/解决方案.txt')
+    # print(r)
